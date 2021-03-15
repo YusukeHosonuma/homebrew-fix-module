adccb5b66cfababc3a96e8b591a3dce915043129
GitHub Action <action@github.com>
2021-03-15 11:43:13 +0000

    update version

--- Formula/fix-module.rb
+++ Formula/fix-module.rb
@@ -1,8 +1,8 @@
 class FixModule < Formula
   desc "A CLI tool that generate struct or class by simple-syntax"
   homepage "https://github.com/YusukeHosonuma/fix-module"
-  url "https://github.com/YusukeHosonuma/fix-module/releases/download/v0.1.1/fix-module"
-  sha256 "fbee1af9bfeb0cd9b143b3cdd19be6a146c6747f023fe5546fcedf288e4da597"
+  url "https://github.com/YusukeHosonuma/fix-module/releases/download/v0.1.0/fix-module"
+  sha256 "4a143f19c2e9326cc38227a79a41706c208732d0bbcf04e8c00e31c75a346230"
   license "MIT"
   
   def install
